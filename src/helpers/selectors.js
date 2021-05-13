f56baf32c2578ee9b3c33383766446e988c71e61
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-13 17:14:02 -0600

    Add and test function to getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,6 @@
 export function getAppointmentsForDay(state, day) {
   let appointments = [];
+
   const filteredAppointments = state.days.filter(dayOfWeek => dayOfWeek.name === day);
   
   if (filteredAppointments.length > 0) {
@@ -7,6 +8,18 @@ export function getAppointmentsForDay(state, day) {
   }
 
   return appointments;
+};
+
+export function getInterviewersForDay(state, day) {
+  let interviewers = [];
+
+  const filteredAppointments = state.days.filter(dayOfWeek => dayOfWeek.name === day);
+  
+  if (filteredAppointments.length > 0) {
+    interviewers = filteredAppointments[0].interviewers.map(interviewerId => state.interviewers[interviewerId]);
+  }
+
+  return interviewers;
 };
 
 export function getInterview(state, interview) {  
