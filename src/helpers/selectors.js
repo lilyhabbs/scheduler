5a1c185761b568167ccaa276e057a9f2069695cb
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-13 13:18:27 -0600

    Add and test function to getInterview data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,14 @@ export function getAppointmentsForDay(state, day) {
 
   return appointments;
 };
+
+export function getInterview(state, interview) {  
+  if (!interview) {
+    return null;
+  }
+
+  return {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer],
+  };
+};
