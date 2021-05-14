29c51939396f4f3bedf8ee57bd3b45925599f09a
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-13 21:27:37 -0600

    Show saving status indicator

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
 import Form from 'components/Appointment/Form';
+import Status from 'components/Appointment/Status';
 import useVisualMode from 'hooks/useVisualMode';
 
 import 'components/Appointment/styles.scss';
@@ -12,12 +13,15 @@ export default function Appointment(props){
   const EMPTY = 'EMPTY';
   const SHOW = 'SHOW';
   const CREATE = 'CREATE';
+  const SAVING = 'SAVING';
 
   const { mode, transition, back } = useVisualMode (
     props.interview ? SHOW : EMPTY
   );
 
   function save(name, interviewer) {
+    transition(SAVING);
+
     const interview = {
       student: name,
       interviewer
@@ -32,18 +36,19 @@ export default function Appointment(props){
   return (
     <article className="appointment">
       <Header time={ props.time } />
-      { mode === EMPTY && <Empty onAdd={ () => transition(CREATE) } /> }
-      { mode === SHOW && (
+      {mode === EMPTY && <Empty onAdd={ () => transition(CREATE) } />}
+      {mode === SHOW && (
         <Show
           student={ props.interview.student }
           interviewer={ props.interview.interviewer }
         />
-      ) }
-      { mode === CREATE && <Form
+      )}
+      {mode === CREATE && <Form
         interviewers={ props.interviewers }
         onCancel={ () => back() }
         onSave={ save }
-      /> }
+      />}
+      {mode === SAVING && <Status message={"Saving"} />}
     </article>
   );
 }
