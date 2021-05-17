09f53dbc27166e10b2a98a2221dfb39dcb3eafa8
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-17 16:27:21 -0600

    Validate interviewers prop

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+
 import InterviewerListItem from 'components/InterviewerListItem';
 
 import 'components/InterviewerList.scss';
@@ -25,3 +27,7 @@ export default function InterviewerList(props){
     </section>
   );
 }
+
+// InterviewerList.propTypes = {
+//   interviewers: PropTypes.array.isRequired
+// };
