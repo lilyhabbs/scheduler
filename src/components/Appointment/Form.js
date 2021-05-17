ee1046b4666ae5eebef6c8a5267e3843955ba94a
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-17 17:25:19 -0600

    Create tests for Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,12 +17,13 @@ export default function Form(props){
     props.onCancel();
   }
 
-  const validate = () => {
+  function validate() {
     if (name === "") {
       setError("Student name cannot be blank");
       return;
     }
   
+    setError("");
     props.onSave(name, interviewer);
   }
 
