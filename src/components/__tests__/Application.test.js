0ba5125fe6c40480f7258f591ed83fb239852ddf
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-18 14:53:43 -0600

    Update test to check spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, fireEvent, waitForElement, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -30,9 +30,18 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
   
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
+  
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
