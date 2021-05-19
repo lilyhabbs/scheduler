06ddf0139aed6e2b6a32c9c5a7e3c68fa843b9cb
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-18 20:25:55 -0600

    Refactor code to fix missing key

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,17 +4,15 @@ import DayListItem from 'components/DayListItem';
 export default function DayList(props){
   const days = props.days.map((day) => {
     return (
-      <ul>
-        <DayListItem
-          key={ day.id }
-          name={ day.name }
-          spots={ day.spots }
-          selected={ day.name === props.day }
-          setDay={ props.setDay }  
-        />
-      </ul>
+      <DayListItem
+        key={ day.id }
+        name={ day.name }
+        spots={ day.spots }
+        selected={ day.name === props.day }
+        setDay={ props.setDay }  
+      />
     )
   });
 
-  return days;
+  return <ul>{ days }</ul>;
 }
