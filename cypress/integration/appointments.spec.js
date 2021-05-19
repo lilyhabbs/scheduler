2f2f8378835e4e811f522085c0c55c6aebb2e611
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-19 16:36:26 -0600

    Add test for deleting an appt

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,8 +22,7 @@ describe("Appointments", () => {
   });
 
   it("should edit an interview", () => {
-    cy.get("[alt=Edit]")
-      .first()
+    cy.get("[alt=Edit]")     
       .click({ force: true });
   
     cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
@@ -33,5 +32,18 @@ describe("Appointments", () => {
   
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+  
+    cy.contains("Confirm").click();
+  
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+  
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 });
